6dde3efbdc737f8a11f4e604b4107edd3dddb336
Patrick <pjr99@cornell.edu>
2025-01-02 10:21:57 -0500

    refactoring

--- coffeeconnect/wsgi.py
+++ coffeeconnect/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffeeconnect.settings')
+os.environ.setdefault('PORT', '8000')
 
 application = get_wsgi_application()
